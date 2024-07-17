e94be09cb26a77efb49f7353c7d2cc3299112a7a
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-07-16 23:50:41 -0500

    feat(api): :alien: Finished fetch managment config

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,28 @@
-import axios from "axios"
+import axios from "axios";
+import Customers from "./customers";
+import Orders from "./orders";
+import Products from "./products";
+import Transactions from "./transactions";
 
 export const api = () => {
-
   const axiosConfig = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
-      'Cache-Control': 'no-cache',
-      'Access-Control-Allow-Origin': '*',
+      "Cache-Control": "no-cache",
+      "Access-Control-Allow-Origin": "*",
     },
-    timeout: 45000
-  })
+    timeout: 45000,
+  });
 
+  const customers = Customers(axiosConfig);
+  const orders = Orders(axiosConfig);
+  const products = Products(axiosConfig);
+  const transactions = Transactions(axiosConfig);
 
-  return {}
-
-}
+  return {
+    customers,
+    orders,
+    products,
+    transactions
+  };
+};
